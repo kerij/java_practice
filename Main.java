5845eafe346dca43435877b6ab27d558457debae
kerij <keriajackson@gmail.com>
2016-08-26 23:10:47 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char letter = 'a';
+        //initialize two ints
+        int x = 1, y = 26;
+
+        while (x <= y) {
+            System.out.print(letter + ",");
+            letter++;
+            x++;
+        }
+
+    }
+}
